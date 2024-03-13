e40b8e92c989fbd50d166792218112d4ff2bca8e
sakuraAK <andrey.krutauz@gmail.com>
2024-03-12 20:46:42 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-//import "./index.css";
-import "./test-style.css";
+import "./index.css";
+//import "./test-style.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
