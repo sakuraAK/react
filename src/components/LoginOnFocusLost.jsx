c9b0fa147ff77348c0d27550b4729d8c80c11ea3
Andrey Krutauz <andrey.krutauz@gmail.com>
2024-05-15 17:31:38 -0400

    Validation on submit init commit

--- src/components/LoginOnFocusLost.jsx
+++ src/components/LoginOnFocusLost.jsx
@@ -8,6 +8,12 @@ export default function Login() {
   const password = useRef();
 
 
+  const [dirty, setDirty] = useState({
+    email: false,
+    password: false,
+  });
+
+
   function handleSubmitEvent(event) {
       event.preventDefault();
       console.log("User email: " + email.current.value);
@@ -23,23 +29,27 @@ export default function Login() {
   
 
 
-  const emailIsInvalid = false;
-  const passwordIsInvalid = false;
+  function handleOnBlur(identifier) {
+    setDirty(prev => ({...prev, [identifier]: true}));
+  }
+
+  const emailIsInvalid = dirty.email && !validation.isEmail(email.current.value);
+  const passwordIsInvalid = dirty.password && (!validation.hasMinLength(password.current.value, 8) || !validation.isNotEmpty(password.current.value));
 
   return (
-    <form>
+    <form onSubmit={handleSubmitEvent}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="text" name="email" ref={email}/>
+          <input onBlur={() => handleOnBlur('email')} id="email" type="email" name="email" ref={email}/>
           { emailIsInvalid && <div className="control-error"><p>Enter valid email address!</p></div>}
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input  id="password"  name="password" ref={password}/>
+          <input onBlur={() => handleOnBlur('password')} id="password" type="password" name="password" ref={password}/>
           { passwordIsInvalid && <div className="control-error"><p>Enter valid password!</p></div>}
         </div>
       </div>
